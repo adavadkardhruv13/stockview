2213c8497117d8cf75bae909d149351caf444c05
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2025-04-02 13:47:53 +0530

    updated database

--- backend/database.py
+++ backend/database.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
-db = client["b2b_auth"]
+db = client["stockviewmain"]
 users_collection = db["users"]
 otp_collection = db["otps"]
+ipo_collection = db["ipos"]
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, HttpUrl, Field
 from typing import Optional
+from datetime import date, datetime
 
 class User(BaseModel):
     email: EmailStr
@@ -12,3 +13,22 @@ class Otp(BaseModel):
     email : EmailStr
     otp: str
     
+    
+class Ipo(BaseModel):
+    category: str  # upcoming, listed, active, closed
+    symbol: str 
+    name: str
+    status: str
+    is_sme: bool
+    additional_text: Optional[str] = None
+    min_price: Optional[float] = None
+    max_price: Optional[float] = None
+    issue_price: Optional[float] = None
+    listing_gains: Optional[float] = None
+    listing_price: Optional[float] = None
+    bidding_start_date: Optional[date] = None
+    bidding_end_date: Optional[date] = None
+    listing_date: Optional[date] = None
+    lot_size: Optional[int] = None
+    # document_url: str  
+    last_updated: datetime = Field(default_factory=datetime.utcnow)
